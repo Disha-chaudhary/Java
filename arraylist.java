cea19ae1986237188a206b05c66b59fe167f00bf
Disha chaudhary <disha.chaudhary_cs24@gla.ac.in>
2025-11-20 21:21:32 -1200

    arraylist

---
+++ arraylist.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class arraylist {
+    public static void main(String[] args) {
+        List<Integer> l1=new ArrayList<>();
+        l1.add(3);
+        l1.add(4);
+        l1.add(9);
+        l1.add(7);
+        // System.out.println(l1);
+        // System.out.println(l1.add(8));
+
+
+        // for(int i=0;i<l1.size();i++){
+        //     System.out.println(l1.get(i));
+        // }
+
+        // l1.add(1,100);
+        // System.out.println(l1);
+        // System.out.println(l1.size());
+
+        //l1.remove(1);//index
+        //REMOVING AN ELEMENT
+        // l1.remove(Integer.valueOf(4));
+        System.out.println(l1);
+        
+
+    }
+}
