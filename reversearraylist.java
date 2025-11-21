116de245f58545b5cd35a359e119a28ecd6f3e45
Disha chaudhary <disha.chaudhary_cs24@gla.ac.in>
2025-11-20 21:29:43 -1200

    reversearraylist

---
+++ reversearraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reversearraylist {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        List<Integer> l1=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            l1.add(val);
+        }
+
+        for(int i=n-1;i>=0;i--){
+            System.out.print(l1.get(i)+" ");
+        }
+            }
+}
