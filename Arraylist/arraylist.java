e80011836d62b06e6962ae68af4531ef90a3340a
Disha Chaudhary <dishachaudhary129@gmail.com>
2026-02-02 21:35:21 +0530

    traversing

--- Arraylist/arraylist.java
+++ Arraylist/arraylist.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 public class arraylist {
     public static void main(String[] args) {
         List<Integer> l1=new ArrayList<>();
